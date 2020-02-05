f14ee561f90a9fce7a8cb2dc1aefa6a14a6cd3bc
uridickman <55203070+uridickman@users.noreply.github.com>
2020-02-05 10:06:52 -0800

    rr4ew

--- Big Canvas/canvas.js
+++ Big Canvas/canvas.js
@@ -1,9 +1,13 @@
 addEventListener("load", init);
 addEventListener('keydown', translateKey);
 
-//++++++++++++Assign global variables
+//++++++++++++Assign global variables for main canvas
 var canvas;
 var context;
+var world = {
+    width: 4000,
+    height: 3000
+};
 //set position of origin (x, y)
 var x;
 var y;
@@ -11,26 +15,59 @@ var y;
 var l1;
 var l2;
 
+//++++++++++++Assign global variables for mini canvas
+var scale = 4;
+var miniContext;
+var miniCanvas;
+var miniWorld = {
+    width: world.width/scale,
+    height: world.height/scale
+};
+//set position of mini origin (miniX, miniY)
+var minX;
+var miniY;
+
+var miniL1;
+var miniL2;
+
 var picture1 = new Image();
 
 //++++++++++++Called onload
 function init(){
-  canvas = document.getElementById("cnv");
-  canvas.width = 800
-  canvas.height = 600;
+    //++++++++++++++++++++++++++++++++++++++main canvas setup
+    canvas = document.getElementById("cnv");
+    canvas.width = 800
+    canvas.height = 600;
 
-  context = canvas.getContext("2d");
-  canvas.style.backgroundColor = "rgb(135,206,250)";
-  picture1.src = "picture1.png";
+    context = canvas.getContext("2d");
+    canvas.style.backgroundColor = "rgb(135,206,250)";
+    picture1.src = "picture1.png";
 
-  x = -canvas.width/2;
-  y = -canvas.height/2;
+    //define origin
+    x = -canvas.width/2;
+    y = -canvas.height/2;
 
-  l1 = new Line(-10000, 0, 10000, 0);
-  l2 = new Line(0, -10000, 0, 10000);
+    //draw axes
+    l1 = new Line(-world.width/2, 0, world.width/2, 0, context, 1);
+    l2 = new Line(0, -world.height/2, 0, world.height/2, context, 1);
 
-  context.translate(0, 0);
-  animate();
+    //++++++++++++++++++++++++++++++++++++++++++minimap canvas setup
+    miniCanvas = document.getElementById("miniCnv");
+    miniCanvas.width = canvas.width/scale;
+    miniCanvas.height = canvas.height/scale;
+
+    miniContext = miniCanvas.getContext("2d");
+    miniCanvas.style.backgroundColor = "rgb(135,206,250)";
+
+    //define origin
+    miniX = -miniCanvas.width/2;
+    miniY = -miniCanvas.height/2;
+
+    //draw axes
+    miniL1 = new Line(-world.width/2, 0, world.width/2, 0, miniContext, .3);
+    miniL2 = new Line(0, -world.height/2, 0, world.height/2, miniContext, .3);
+    
+    animate();
 }
 
 //++++++++++++Called on keydown
@@ -40,26 +77,31 @@ function translateKey(key){
         //key A
         case 65:
             x -= translateRate;
+            // miniX -= translateRate/scale;
             break;
         //key W
         case 87:
             y -= translateRate;
+            // miniY -= translateRate/scale;
             break;
         //key D
         case 68:
             x += translateRate;
+            // miniX += translateRate/scale;
             break;
         //key S
         case 83:
             y += translateRate;
+            // miniY += translateRate/scale;
             break;
         default: console.log("Incorrect key. Try again.");
-    } 
+    }
 }
 
 function animate(){
     requestAnimationFrame(animate);
-    context.clearRect(-10000, -10000, 20000, 20000);
+    //+++++++++++++++++++++++++++++Render main context
+    context.clearRect(-world.width/2, -world.height/2, world.width, world.height);
 
     context.save();
 
@@ -77,4 +119,31 @@ function animate(){
     context.drawImage(picture1, 3*canvas.width/4, 3*canvas.height/4, 100, 100);
 
     context.restore();
+
+    //++++++++++++++++++++++++++++++++++Render mini context
+    miniContext.clearRect(-miniWorld.width/2, -miniWorld.height/2, miniWorld.width, miniWorld.height);
+
+    miniContext.save();
+
+    miniContext.scale(miniCanvas.width/world.width, miniCanvas.height/world.height);
+    //translate canvas by (-x, -y) that have been incremented in translateKey(key)
+    miniContext.translate(world.width/2, world.width/2 - 500);
+
+    //draw lines
+    miniL1.run();
+    miniL2.run();
+
+    miniContext.beginPath();
+    miniContext.lineWidth = 25;
+    miniContext.strokeStyle = "black";
+    miniContext.rect(x, y, canvas.width, canvas.height);
+    miniContext.stroke();
+
+    //draw 4 images in their resepctive positions in the world 
+    miniContext.drawImage(picture1, -3*miniCanvas.width/4, -3*miniCanvas.height/4, 100/scale*2, 100/scale*2); 
+    miniContext.drawImage(picture1, -3*miniCanvas.width/4, 3*miniCanvas.height/4, 100/scale*2, 100/scale*2);
+    miniContext.drawImage(picture1, 3*miniCanvas.width/4, -3*miniCanvas.height/4, 100/scale*2, 100/scale*2);
+    miniContext.drawImage(picture1, 3*miniCanvas.width/4, 3*miniCanvas.height/4, 100/scale*2, 100/scale*2);
+
+    miniContext.restore();
 }
--- Big Canvas/lines.js
+++ Big Canvas/lines.js
@@ -1,17 +1,19 @@
-function Line(x1, y1, x2, y2){
+function Line(x1, y1, x2, y2, context, scale){
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+    this.ctx = context;
+    this.scale = scale;
   }
   
   Line.prototype.draw = function(){
-    context.beginPath();
-    context.lineWidth = 6;
-    context.strokeStyle = "rgb(221, 65, 36)";
-    context.moveTo(this.x1, this.y1);
-    context.lineTo(this.x2, this.y2);
-    context.stroke();
+    this.ctx.beginPath();
+    this.ctx.lineWidth = 6/this.scale;
+    this.ctx.strokeStyle = "rgb(221, 65, 36)";
+    this.ctx.moveTo(this.x1, this.y1);
+    this.ctx.lineTo(this.x2, this.y2);
+    this.ctx.stroke();
   }
   
   Line.prototype.run = function(){
