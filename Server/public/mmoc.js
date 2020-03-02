16e6151d318d1f331d722a20e27f706b2f05d6a3
uridickman <55203070+uridickman@users.noreply.github.com>
2020-03-02 10:23:49 -0800

    33

--- Server/public/mmoc.js
+++ Server/public/mmoc.js
@@ -1,10 +1,19 @@
 function MMOC() {
     this.ws;
-    this.id;
+    this.id = "";
+    this.connected = false;
+
+    this.init();
 }
 
 MMOC.prototype.init = function () {
 
+
+    this.ws = new WebSocket();
+
+    this.ws.onopen = function(event){
+        console.log("websocket is open now");
+    }
 }
 
 MMOC.prototype.sendGreeting = function () {
