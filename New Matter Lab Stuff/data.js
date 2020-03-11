c0242dbd840d50108dd925d20fa6f9f5598f92cf
uridickman <55203070+uridickman@users.noreply.github.com>
2020-03-10 19:07:21 -0700

    sdas

---
+++ New Matter Lab Stuff/data.js
@@ -0,0 +1,68 @@
+var initialWorld = {
+    "players": [
+        "player1": {
+            "bodies": [
+                {
+                    "name": "body1",
+                    "type": "rock",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "radius": ""
+                },
+                {
+                    "name": "body2",
+                    "type": "pyramid",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "blockSize": {
+                        "width": "",
+                        "height": ""
+                    },
+                    "numRows": ""
+                },
+            ],
+            "constraints": {
+                "name": "toAnchor1",
+                "pointA": "",
+                "bodyB": "",
+                "length": ""
+            }
+        },
+        "player2": {
+            "bodies": [
+                {
+                    "name": "body1",
+                    "type": "rock",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "radius": ""
+                },
+                {
+                    "name": "body2",
+                    "type": "pyramid",
+                    "position": {
+                        "x": "",
+                        "y": ""
+                    },
+                    "blockSize": {
+                        "width": "",
+                        "height": ""
+                    },
+                    "numRows": ""
+                },
+            ],
+            "constraints": {
+                "name": "toAnchor1",
+                "pointA": "",
+                "bodyB": "",
+                "length": ""
+            }
+        }
+    ]
+}
